5086d086e6806474c1d6cbe61189aeab07ad7950
Lean.Tvener <lean.tvener@ombrello.io>
2025-03-20 22:41:52 +0100

    adding instruction

--- app/hooks/feedback.test.ts
+++ app/hooks/feedback.test.ts
@@ -1,7 +1,7 @@
 import { it, expect } from 'vitest'
 import feedback from './feedback'
 
-
+//in the terminal run:   npx vitest -no-watch --coverage
 it('should return feedback', () => {
     expect(feedback('feedback')).toBe('your doing great')
 })
