fc5570b6e5f5dd2ac2507e03ba40f5dc940f8f29
Lean.Tvener <lean.tvener@ombrello.io>
2025-03-20 22:21:18 +0100

    UtilityExample setup

--- app/Components/ImageWithLink/ImageWithLink.test.tsx
+++ app/Components/ImageWithLink/ImageWithLink.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest'
 import { userEvent } from '@testing-library/user-event'
-import ImageWithLink from './ImageWithLink'
+
 
 test('renders ImageWithLink component', async () => {
+    const mockFunction = vi.fn()
     const user = userEvent.setup()
-    await render(<ImageWithLink image={{ src: 'https://via.placeholder.com/150', alt: 'Image', width: 150, height: 150 }} linkUrl="https://www.google.com" linkText="Link" creditText="Credit" />)
-    await user.click(screen.getByText('Link'))
-    expect(screen.getByText('Link')).toBeInTheDocument()
+    //screen.debug()
+    await render(<div><button onClick={mockFunction} role="button">Click me</button></div>)
+    //screen.debug()
+    //screen.logTestingPlaygroundURL()
+    await user.click(screen.getByText('Click me'))
+    expect(mockFunction).toHaveBeenCalledTimes(1)
+
 })
