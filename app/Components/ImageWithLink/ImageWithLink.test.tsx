ce86c9f2dca6d643aec2ce601100d3cc231ec76e
Lean.Tvener <lean.tvener@ombrello.io>
2025-03-21 11:26:31 +0100

    minor update

--- app/Components/ImageWithLink/ImageWithLink.test.tsx
+++ app/Components/ImageWithLink/ImageWithLink.test.tsx
@@ -5,7 +5,7 @@ import ImageWithLink from './ImageWithLink'
 
 test('renders ImageWithLink component', async () => {
     const user = userEvent.setup()
-    await render(<ImageWithLink image={{ src: 'https://via.placeholder.com/150', alt: 'Image', width: 150, height: 150 }} linkUrl="https://www.google.com" linkText="Link" creditText="Credit" />)
+    await render(<ImageWithLink image={{ src: 'testurl.png', alt: 'Image', width: 150, height: 150 }} linkUrl="https://www.google.com" linkText="Link" creditText="Credit" />)
     await user.click(screen.getByText('Link'))
-    expect(screen.getByText('Link')).toBeInTheDocument()
+    expect(screen.getByText('Link')).toBeVisible()
 })
